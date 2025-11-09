d044435f6167f1fe1ea027b571260950d2e431b4
aryafmdev <muhammadaryafm@gmail.com>
2025-11-09 16:13:57 +0700

    feat: add some updated files

---
+++ PracticeCodePart2/leetcodeMixCodes.js
@@ -0,0 +1,50 @@
+// 1. [#2667] Create Hello World Function
+// https://leetcode.com/problems/create-hello-world-function/
+// const createHelloWorld = () =>
+//   () => "Hello World"; 
+
+// 2. [#2620] Counter
+// https://leetcode.com/problems/counter/
+// const createCounter = (n) => () => n++;
+
+// 3. [#2666] Allow One Function Call
+// https://leetcode.com/problems/allow-one-function-call/
+// const once = (fn) => {
+//   let called = false;
+//   return (...args) => {
+//     if (called) return undefined;
+//     called = true;
+//     return fn(...args);
+//   };
+// };
+
+// 4. [#2704] To Be Or Not To Be
+// https://leetcode.com/problems/to-be-or-not-to-be/
+// const expect = (val) => ({
+//   toBe: (v) => {
+//     if (val !== v) throw new Error("Not Equal");
+//     return true;
+//   },
+//   notToBe: (v) => {
+//     if (val === v) throw new Error("Equal");
+//     return true;
+//   },
+// });
+
+// 5. [#2715] Expect Exception
+// https://leetcode.com/problems/expect-exception/
+
+// 6. [#2703] Return Length of Arguments Passed
+// https://leetcode.com/problems/return-length-of-arguments-passed/
+
+// 7. [#2665] Create Counter II
+// https://leetcode.com/problems/create-counter-ii/
+
+// 8. [#2635] Apply Transform Over Each Element in Array
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/
+
+// 9. [#2727] Make Hello World Function Call N Times
+// https://leetcode.com/problems/make-hello-world-function-call-n-times/
+
+// 10. [#2723] Add Two Promises
+// https://leetcode.com/problems/add-two-promises/
---
+++ PracticeCodePart2/reps01.js
@@ -0,0 +1,49 @@
+// 01 hello world
+// console.log(parameterVariable);
+
+// 02 data types
+// console.log(firstInteger + Number(secondInteger));
+// console.log(firstDecimal + Number(secondDecimal));
+// console.log(firstString + secondString);
+
+// 03 arithmetic operators
+// const getArea = (length, width) => length * width;
+// const getPerimeter = (length, width) => 2 * (length + width);
+
+// 04 functions - factorial
+// const factorial = n => n === 0 ? 1 : n * factorial(n - 1);
+
+// 05 let & const
+// const PI = Math.PI;
+// const r = +readline();
+// const area = PI * (r**2);
+// const perimeter = 2 * PI * r;
+// // Print the area of the circle:
+// console.log(area);
+// // Print the perimeter of the circle:
+// console.log(perimeter);
+
+// 06 conditional statement if-else
+// const getGrade = score =>
+//   score >= 0 && score <= 5 ? 'F' :
+//   score <= 10 ? 'E' :
+//   score <= 15 ? 'D' :
+//   score <= 20 ? 'C' :
+//   score <= 25 ? 'B' :
+//   score <= 30 ? 'A' : '';
+
+// 07 conditional statement switch (yang di cek INDEX AWAL)
+// const getLetter = s =>
+//   'aeiou'.includes(s[0]) ? 'A' :
+//   'bcdfg'.includes(s[0]) ? 'B' :
+//   'hjklm'.includes(s[0]) ? 'C' : 'D';
+
+// 08 loops
+
+// 09 arrays
+
+// 10 try catch finally
+
+// 11 throw
+
+// 12 create a rectangle object
