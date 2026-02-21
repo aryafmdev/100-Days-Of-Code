6c977cfa48de62606a30fb97cf9c15a6bdf45dfd
aryafmdev <muhammadaryafm@gmail.com>
2026-02-21 10:02:09 +0700

    feat(pascal-triangle): add solution for LeetCode 119 with O(rowIndex) space optimization in getRow.js

--- PracticeCodePart2/leetcodeMixCodes.js
+++ PracticeCodePart2/leetcodeMixCodes.js
@@ -6992,3 +6992,16 @@
 //     right--;
 //   }
 // };
+
+// 119. Pascal's Triangle II
+
+// const getRow = (rowIndex) => {
+//   const row = [1];
+//   for (let i = 1; i <= rowIndex; i++) {
+//     row.push(1);
+//     for (let j = i - 1; j > 0; j--) {
+//       row[j] += row[j - 1];
+//     }
+//   }
+//   return row;
+// };
